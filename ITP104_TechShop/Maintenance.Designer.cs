04a4554ee310b8cf1f0c9c64179c5d871065773a
Harold Galang <galangharold56@gmail.com>
2023-12-12 14:23:13 +0800

    12/12/2023 2:22 PM Update

--- ITP104_TechShop/Maintenance.Designer.cs
+++ ITP104_TechShop/Maintenance.Designer.cs
@@ -28,18 +28,416 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnSetUser = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            groupBox1 = new GroupBox();
+            button6 = new Button();
+            button5 = new Button();
+            button1 = new Button();
+            dgvMaintenance = new DataGridView();
+            groupBox2 = new GroupBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            btnShowTblUser = new Button();
+            button7 = new Button();
+            groupBox3 = new GroupBox();
+            label2 = new Label();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            groupBox4 = new GroupBox();
+            label3 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvMaintenance).BeginInit();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
             SuspendLayout();
+            // 
+            // btnSetUser
+            // 
+            btnSetUser.Location = new Point(6, 155);
+            btnSetUser.Name = "btnSetUser";
+            btnSetUser.Size = new Size(59, 26);
+            btnSetUser.TabIndex = 1;
+            btnSetUser.Text = "Set User";
+            btnSetUser.UseVisualStyleBackColor = true;
+            btnSetUser.Click += btnSetUser_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 110);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(6, 69);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(btnSetUser);
+            groupBox1.Controls.Add(btnShowTblUser);
+            groupBox1.Location = new Point(12, 220);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(219, 187);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Set Users";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(71, 155);
+            button6.Name = "button6";
+            button6.Size = new Size(74, 26);
+            button6.TabIndex = 9;
+            button6.Text = "Set User";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(151, 155);
+            button5.Name = "button5";
+            button5.Size = new Size(59, 26);
+            button5.TabIndex = 8;
+            button5.Text = "Set User";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 26);
+            button1.TabIndex = 7;
+            button1.Text = "Show Table";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dgvMaintenance
+            // 
+            dgvMaintenance.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvMaintenance.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMaintenance.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dgvMaintenance.Location = new Point(242, 220);
+            dgvMaintenance.Name = "dgvMaintenance";
+            dgvMaintenance.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvMaintenance.Size = new Size(451, 187);
+            dgvMaintenance.TabIndex = 16;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button7);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(219, 187);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Set Item Category";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(8, 57);
+            label4.Name = "label4";
+            label4.Size = new Size(24, 15);
+            label4.TabIndex = 17;
+            label4.Text = "ID: ";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 98);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(74, 26);
+            button2.TabIndex = 9;
+            button2.Text = "Set User";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(151, 155);
+            button3.Name = "button3";
+            button3.Size = new Size(59, 26);
+            button3.TabIndex = 8;
+            button3.Text = "Set User";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // btnShowTblUser
+            // 
+            btnShowTblUser.Location = new Point(0, 22);
+            btnShowTblUser.Name = "btnShowTblUser";
+            btnShowTblUser.Size = new Size(118, 26);
+            btnShowTblUser.TabIndex = 7;
+            btnShowTblUser.Text = "Show Table";
+            btnShowTblUser.UseVisualStyleBackColor = true;
+            btnShowTblUser.Click += btnShowTblUser_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(86, 155);
+            button7.Name = "button7";
+            button7.Size = new Size(59, 26);
+            button7.TabIndex = 1;
+            button7.Text = "Set User";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label2);
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Controls.Add(button8);
+            groupBox3.Controls.Add(button9);
+            groupBox3.Controls.Add(button10);
+            groupBox3.Controls.Add(button11);
+            groupBox3.Location = new Point(474, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(219, 187);
+            groupBox3.TabIndex = 13;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Set Suppliers";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 15);
+            label2.TabIndex = 14;
+            label2.Text = "ID: ";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(6, 69);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(204, 23);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(6, 98);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(204, 23);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 127);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(204, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(71, 156);
+            button8.Name = "button8";
+            button8.Size = new Size(74, 26);
+            button8.TabIndex = 9;
+            button8.Text = "Set User";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(151, 156);
+            button9.Name = "button9";
+            button9.Size = new Size(59, 26);
+            button9.TabIndex = 8;
+            button9.Text = "Set User";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(0, 22);
+            button10.Name = "button10";
+            button10.Size = new Size(118, 26);
+            button10.TabIndex = 7;
+            button10.Text = "Show Table";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(6, 156);
+            button11.Name = "button11";
+            button11.Size = new Size(59, 26);
+            button11.TabIndex = 1;
+            button11.Text = "Set User";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label3);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(textBox9);
+            groupBox4.Controls.Add(button12);
+            groupBox4.Controls.Add(button13);
+            groupBox4.Controls.Add(button14);
+            groupBox4.Controls.Add(button15);
+            groupBox4.Location = new Point(249, 12);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(219, 187);
+            groupBox4.TabIndex = 15;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Set Suppliers";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 51);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 15);
+            label3.TabIndex = 14;
+            label3.Text = "ID: ";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(6, 69);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(204, 23);
+            textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(6, 98);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(204, 23);
+            textBox8.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(6, 127);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(204, 23);
+            textBox9.TabIndex = 11;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(71, 156);
+            button12.Name = "button12";
+            button12.Size = new Size(74, 26);
+            button12.TabIndex = 9;
+            button12.Text = "Set User";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(151, 156);
+            button13.Name = "button13";
+            button13.Size = new Size(59, 26);
+            button13.TabIndex = 8;
+            button13.Text = "Set User";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(0, 22);
+            button14.Name = "button14";
+            button14.Size = new Size(118, 26);
+            button14.TabIndex = 7;
+            button14.Text = "Show Table";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(6, 156);
+            button15.Name = "button15";
+            button15.Size = new Size(59, 26);
+            button15.TabIndex = 1;
+            button15.Text = "Set User";
+            button15.UseVisualStyleBackColor = true;
             // 
             // frmMaintenance
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(702, 416);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(dgvMaintenance);
+            Controls.Add(groupBox1);
             Name = "frmMaintenance";
             Text = "Maintenance";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvMaintenance).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnSetUser;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private DataGridView dgvMaintenance;
+        private Button button6;
+        private Button button5;
+        private GroupBox groupBox2;
+        private Button button2;
+        private Button button3;
+        private Button btnShowTblUser;
+        private Button button7;
+        private GroupBox groupBox3;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private TextBox textBox3;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label4;
+        private GroupBox groupBox4;
+        private Label label3;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
     }
 }
--- ITP104_TechShop/Maintenance.cs
+++ ITP104_TechShop/Maintenance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,49 @@ namespace ITP104_TechShop
 {
     public partial class frmMaintenance : Form
     {
+        String con;
         public frmMaintenance()
         {
             InitializeComponent();
+            con = "Server=localhost;Database=db_TechShop; User = root; Password =1234; ";
+            MySqlConnection connection = new MySqlConnection(con);
+        }
+
+        private void btnSetUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowTblUser_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = null;
+            try
+            {
+                connection = new MySqlConnection(con);
+                connection.Open();
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "Select * From tblUsers";
+                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                dgvMaintenance.DataSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception z)
+            {
+                MessageBox.Show("Connection Problem");
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
